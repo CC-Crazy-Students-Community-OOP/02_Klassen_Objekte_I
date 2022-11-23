0b28cdf5109ecf3bcb65678944a4ee1be50f6216
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-23 09:28:43 +0100

    cat values

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,16 +1,55 @@
 package com.cc.java;
 
-public class App {
-    
-    public static void main(String[] args) {
-        output( "Hallo Klasse" );
+public class App {                                                          // Klasse
+    public static void main( String[] args ) {                              // main method calls the method output
+
+        Cat cat = new Cat();                                                // ertselle ein Objekt / Instanz
+
+        cat.name = "Beauty";
+        cat.color = "black";
+        cat.age = 3;
+
+        // output( cat.name );
+        // output( cat.color );
+        // output( String.valueOf( cat.age ) );
+
+        output( cat.tellYourName() );                                       // kann mir als output hier ausgegeben werden
+        output( cat.tellYourColor() );
+        cat.tellYourAge();                                                  // kann mir als output von aussen ausgegeben werden
+        
+
+        // Beispiele als Übung
+            // output("----------------------");
+            // output( "Die zahl lautet: " + 2 );                           // Erstes Beispiel: kombiniere zahlen mit einem String
+            // output( 2 );                                                 // Oder deklariere eine Methode für einen INT Output, sihe unten
+            // output( String.valueOf( 2 ) );                               // Oder deklariert typeof string
+            // output( Integer.toString( 2 ) );                             // Oder konvertiert int zu string
+            // output("----------------------");
+            // output( "watch from outside to Cat: " + cat );
+            // output("----------------------");
+            // Cat cat1= new Cat();                                         // cat = Rreferenzvariable (enthält die Adresse)
+            // output( "watch from outside to Cat: " + cat1 );
+            // output("----------------------");
+            // // cat.tellYourAdress();                                     // entweder per neu definiertes Objekt
+            // cat.tellYourAdress();                                        // oder per Klasse direkt, dann muss die Klasse aber auch static sein
+            // tellYourAdress();
     }
 
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
+    // public static void tellYourAdress(){                                 // Methoden sind Funktionen in einer Klasse
+    //     System.out.println( "Hey" );
+    // }
+
+
+    public static void output( String outputStr ) {                         // Methoden können als String deklariert werden
+        System.out.println( outputStr );
+    }
+    public static void output( int outputInt ) {                           // methods are function in classes, methods also has to be declard as int                 
+        System.out.println( outputInt );
+    }
+    public static void output( float outputStr ) {                          // methods are function in classes, methods also has to be declard as float
+        System.out.println( outputStr );
     }
 
-
-
+    // public void test1() { System.out.println( "WER1" ); }                // methods zum Testen in Klasse Cat
+    // public static void test2() { System.out.println( "WER2" ); }         // methods zum Testen in Klasse Cat
 }
-
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,21 @@
+package com.cc.java;
+
+public class Cat {
+    public String name;
+    public String color;
+    public int age;
+
+    public String tellYourName() { return this.name; }                      // man kann mit return den Wert weitergeben an einen output
+    public String tellYourColor() { return this.color; }
+    public void tellYourAge() { App.output( this.age ); }                   // man kann mit void, also ohne return den Wert direkt als Ausgabe
+
+    // public void tellYourAdress() {                                       // wenn hier kein public oder private deklariert, dann nimmt er immer die art der Klasse
+    //     // System.out.println( "watch from inside: " + this );           // this ist eine Instanzvariable und ist an ein Objekt gebunden
+    //     App.output( "watch from inside: " + this );                      // this sieht hier obhne static, dass aus einem Objekt gearbeitet wird, daher ergibt this hier ein Output
+
+    //     // App app = new App();
+    //     // app.test1();                                                  // ausgabe ohne static aus App, also aufruf aus dem Objekt app
+    //     // App.test2();                                                  // statis ausgabe aus App, also aufruf aus der Klasse App
+
+    // }
+}
